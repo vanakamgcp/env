64bec40bd9230df19d1f642d745a39c19238b9bc
vanakamgcp <vanakamgcp@gmail.com>
2026-03-22 23:06:59 +0530

    Add IAM roles for developers in main.tf

--- dev/devops/iam/main.tf
+++ dev/devops/iam/main.tf
@@ -1,1 +1,13 @@
+resource "google_project_iam_member" "developer_roles" {
+  for_each = {
+    for pair in setproduct(var.developers, var.roles) :
+    "${pair[0]}-${pair[1]}" => {
+      user = pair[0]
+      role = pair[1]
+    }
+  }
 
+  project = var.project_id
+  role    = each.value.role
+  member  = "user:${each.value.user}"
+}
